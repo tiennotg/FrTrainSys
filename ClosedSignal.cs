4e72c000a46a717dbddc5c5ca43be36af2d67315
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-01-29 23:53:18 +0100

    Add implementations of abstract functions.

--- ClosedSignal.cs
+++ ClosedSignal.cs
@@ -1,9 +1,23 @@
 using System;
-namespace Plugin
+
+namespace FrTrainSys
 {
 	public class ClosedSignal: FrTrainDevice
 	{
-		public ClosedSignal ()
+		public ClosedSignal (TrainSoundManager soundManager,
+		                     TrainHandleManager handleManager): base(soundManager,handleManager)
+		{
+		}
+
+		public override void elapse (OpenBveApi.Runtime.ElapseData data)
+		{
+		}
+
+		public override void reset ()
+		{
+		}
+
+		public override void trainEvent (TrainEvent _event)
 		{
 		}
 	}
