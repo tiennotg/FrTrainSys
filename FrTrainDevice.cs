1eb4337061b34c30abc0b8d8c77210b2b1e4555a
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-01-29 22:53:37 +0100

    Add constructor and train managers.

--- FrTrainDevice.cs
+++ FrTrainDevice.cs
@@ -3,8 +3,14 @@ namespace FrTrainSys
 {
 	abstract public class FrTrainDevice
 	{
-		public FrTrainDevice ()
+		protected TrainSoundManager soundManager;
+		protected TrainHandleManager handleManager;
+
+		public FrTrainDevice (TrainSoundManager soundManager,
+		                      TrainHandleManager handleManager)
 		{
+			this.soundManager = soundManager;
+			this.handleManager = handleManager;
 		}
 	}
 }
