3f642a845137cc412120fe99d8a10d9485771f3f
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-01-29 23:37:12 +0100

    Add SoundIndex enum.

--- TrainSoundManager.cs
+++ TrainSoundManager.cs
@@ -5,6 +5,11 @@ namespace FrTrainSys
 {
 	public class TrainSoundManager
 	{
+		public enum SoundIndex
+		{
+			Vacma, VacmaRing, Beep, KvbOverSpeed, KvbClosedSignal
+		};
+
 		private OpenBveApi.Runtime.PlaySoundDelegate playSound;
 
 		public TrainSoundManager (OpenBveApi.Runtime.PlaySoundDelegate playSound)
