29c6ba3ea74b58b8bbe5345986a1f547be8b2ed7
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-01-29 23:05:34 +0100

    Add implementation of abstract method elapse.

--- Vacma.cs
+++ Vacma.cs
@@ -1,10 +1,16 @@
 using System;
+using OpenBveApi;
+
 namespace FrTrainSys
 {
 	public class Vacma: FrTrainDevice
 	{
 		public Vacma (TrainSoundManager soundManager,
 		              TrainHandleManager handleManager): base(soundManager, handleManager)
+		{
+		}
+
+		public override void elapse (OpenBveApi.Runtime.ElapseData data)
 		{
 		}
 	}
