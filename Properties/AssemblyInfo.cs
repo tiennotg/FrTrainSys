fd9cb3e57c5086c3787ceaccc49bb534ad4cfff3
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-01-29 16:13:03 +0100

    Plugin info.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FrTrainSys")]
+[assembly: AssemblyDescription("Plugin for French security devices.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Guilhem Tiennot")]
+[assembly: AssemblyProduct("FrTrainSys")]
+[assembly: AssemblyCopyright("GNU General Public License 2")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("0.1")]
