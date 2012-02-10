10bf3e88e9dd18672e4d746c58ec28d2f2f370e3
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-10 19:14:10 +0100

    Add closed signal event handle.

--- ClosedSignal.cs
+++ ClosedSignal.cs
@@ -4,6 +4,9 @@ namespace FrTrainSys
 {
 	public class ClosedSignal: FrTrainDevice
 	{
+		private const int signalCrossingDistance = 5;
+		private const int signalAspectForConsideringClosed = 3;
+
 		public ClosedSignal (TrainSoundManager soundManager,
 		                     TrainHandleManager handleManager): base(soundManager,handleManager)
 		{
@@ -24,6 +27,18 @@ namespace FrTrainSys
 
 		public override void trainEvent (TrainEvent _event)
 		{
+			if (_event.getEventType() == EventTypes.EventTypeChangeSignalAspect)
+			{
+				OpenBveApi.Runtime.SignalData[] signal = (OpenBveApi.Runtime.SignalData[]) _event.getEventData();
+
+				/* The first condition ensures that the train is really crossing a signal, and
+				 * that is not an other signal whose aspect is changing. */
+				if (System.Math.Abs(signal[0].Distance) < signalCrossingDistance
+				    && signal[0].Aspect <= signalAspectForConsideringClosed)
+				{
+					this.beep();
+				}
+			}
 		}
 	}
 }
