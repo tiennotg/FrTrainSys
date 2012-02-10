3113e20c8f70e61b9e56e844095d5b989db3e428
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-10 20:41:07 +0100

    Add emergency brake applying when the driver doesn't acknowledge the signal.

--- ClosedSignal.cs
+++ ClosedSignal.cs
@@ -6,28 +6,44 @@ namespace FrTrainSys
 	{
 		private const int signalCrossingDistance = 5;
 		private const int signalAspectForConsideringClosed = 3;
+		private const OpenBveApi.Runtime.VirtualKeys ackKey = OpenBveApi.Runtime.VirtualKeys.A1;
+		private const int tempo = 2500; /* Time before applying emergency brakes (ms) */
+
+		private bool closedSignal;
+		private OpenBveApi.Runtime.Time globalTime;
+		private OpenBveApi.Runtime.Time time;
 
 		public ClosedSignal (TrainSoundManager soundManager,
 		                     TrainHandleManager handleManager): base(soundManager,handleManager)
 		{
+			globalTime = new OpenBveApi.Runtime.Time(0);
+			time = new OpenBveApi.Runtime.Time(0);
+			this.reset();
 		}
 
 		private void beep ()
 		{
-			soundManager.playSoundOnce((int) TrainSoundManager.SoundIndex.Beep);
+			soundManager.playSoundOnce(TrainSoundManager.SoundIndex.Beep);
 		}
 
 		private void crossingClosedSignal ()
 		{
 			this.beep();
+			closedSignal = true;
+			time = globalTime;
 		}
 
 		public override void elapse (OpenBveApi.Runtime.ElapseData data)
 		{
+			globalTime = data.TotalTime;
+
+			if (closedSignal && (globalTime.Milliseconds - time.Milliseconds) > tempo)
+				handleManager.applyEmergencyBrake();
 		}
 
 		public override void reset ()
 		{
+			closedSignal = false;
 		}
 
 		public override void trainEvent (TrainEvent _event)
@@ -42,6 +58,13 @@ namespace FrTrainSys
 				if (System.Math.Abs(signal[0].Distance) < signalCrossingDistance
 				    && signal[0].Aspect <= signalAspectForConsideringClosed)
 					this.crossingClosedSignal();
+			}
+
+			if (_event.getEventType() == EventTypes.EventTypeKeyDown)
+			{
+				OpenBveApi.Runtime.VirtualKeys key = (OpenBveApi.Runtime.VirtualKeys) _event.getEventData();
+				if (key == ackKey)
+					this.reset();
 			}
 		}
 	}
