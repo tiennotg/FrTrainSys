fdcfb5f018dafd02b8ab716bafd15718eac9bb8c
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-10 21:07:37 +0100

    Add handle management and applyEmergencyBrake method.

--- TrainHandleManager.cs
+++ TrainHandleManager.cs
@@ -3,8 +3,31 @@ namespace FrTrainSys
 {
 	public class TrainHandleManager
 	{
-		public TrainHandleManager ()
+		private OpenBveApi.Runtime.VehicleSpecs specs;
+		private OpenBveApi.Runtime.Handles handles;
+		private bool applyHandles;
+
+		public TrainHandleManager (OpenBveApi.Runtime.VehicleSpecs specs)
 		{
+			this.specs = specs;
+			handles = new OpenBveApi.Runtime.Handles(0,0,0,false);
+			applyHandles = false;
+		}
+
+		public void elapse (ref OpenBveApi.Runtime.ElapseData data)
+		{
+			if (applyHandles)
+			{
+				data.Handles = this.handles;
+				applyHandles = false;
+			}
+		}
+
+		public void applyEmergencyBrake ()
+		{
+			applyHandles = true;
+			handles.BrakeNotch = specs.BrakeNotches+1;
+			handles.PowerNotch = 0;
 		}
 	}
 }
