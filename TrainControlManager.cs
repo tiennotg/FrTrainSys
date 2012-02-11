218c1060b09c1d24bc1058b3f8afc7c3c2f79982
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-11 14:28:42 +0100

    Add elapse method.

--- TrainControlManager.cs
+++ TrainControlManager.cs
@@ -5,13 +5,19 @@ namespace FrTrainSys
 	{
 		public const int cabControlCount = 256;
 		private int[] panel;
+		private OpenBveApi.Runtime.Time globalTime;
 
-		public TrainControlManager (ref int[] panel)
+		public TrainControlManager (ref OpenBveApi.Runtime.LoadProperties properties)
 		{
 			panel = new int[cabControlCount];
 			for (int i=0; i<cabControlCount; i++)
 				panel[i] = 0;
-			this.panel = panel;
+			properties.Panel = panel;
+		}
+		
+		public void elapse (OpenBveApi.Runtime.ElapseData data)
+		{
+			globalTime = data.TotalTime;
 		}
 	}
 }
