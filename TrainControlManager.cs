e0eb7a66ee42dafdccaf0007615db6ba2dc1bf4e
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-12 12:53:19 +0100

    Add new cabControls class for listing various controls.

--- TrainControlManager.cs
+++ TrainControlManager.cs
@@ -1,16 +1,23 @@
 using System;
 namespace FrTrainSys
 {
+	/* list of cab controls */
+	public class cabControls
+	{
+		public static const int count = 256;
+		
+		public static const int LSSF = 8;
+	}
+	
 	public class TrainControlManager
 	{
-		public const int cabControlCount = 256;
 		private int[] panel;
 		private OpenBveApi.Runtime.Time globalTime;
 
 		public TrainControlManager (ref OpenBveApi.Runtime.LoadProperties properties)
 		{
-			panel = new int[cabControlCount];
-			for (int i=0; i<cabControlCount; i++)
+			panel = new int[cabControls.count];
+			for (int i=0; i<cabControls.count; i++)
 				panel[i] = 0;
 			properties.Panel = panel;
 		}
