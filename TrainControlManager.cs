ec3cf1d09a0bc61315024306e16584128270fafd
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-12 15:39:06 +0100

    Add Method to set blinking to a control.

--- TrainControlManager.cs
+++ TrainControlManager.cs
@@ -1,21 +1,30 @@
 using System;
+using System.Collections.Generic;
+
 namespace FrTrainSys
 {
 	/* list of cab controls */
-	public class cabControls
+	static class cabControls
 	{
-		public static const int count = 256;
+		public static int count = 256;
 		
-		public static const int LSSF = 8;
+		public static int LSSF = 10;
 	}
 	
 	public class TrainControlManager
 	{
 		private int[] panel;
+		
+		private List<int> controlBlinking;
 		private OpenBveApi.Runtime.Time globalTime;
+		private OpenBveApi.Runtime.Time lastTime;
+		private int halfBlinkingPeriod = 100; /* (in ms) duration of a control state while blinking */
 
 		public TrainControlManager (ref OpenBveApi.Runtime.LoadProperties properties)
 		{
+			controlBlinking = new List<int>();
+			lastTime = new OpenBveApi.Runtime.Time(0);
+			
 			panel = new int[cabControls.count];
 			for (int i=0; i<cabControls.count; i++)
 				panel[i] = 0;
@@ -25,6 +34,17 @@ namespace FrTrainSys
 		public void elapse (OpenBveApi.Runtime.ElapseData data)
 		{
 			globalTime = data.TotalTime;
+			
+			if (globalTime.Milliseconds - lastTime.Milliseconds > halfBlinkingPeriod)
+			{
+				controlBlinking.ForEach(delegate (int control) {
+					if (getState(control) == 1)
+						setState(control, 0);
+					else
+						setState(control, 1);
+				});
+				lastTime = globalTime;
+			}
 		}
 		
 		public int getState (int control)
@@ -35,6 +55,18 @@ namespace FrTrainSys
 		public void setState (int control, int state)
 		{
 			panel[control] = state;
+		}
+		
+		public void startBlinking (int control)
+		{
+			if (!controlBlinking.Contains(control))
+				controlBlinking.Add(control);
+		}
+		
+		public void stopBlinking (int control)
+		{
+			if (controlBlinking.Contains(control))
+				controlBlinking.Remove(control);
 		}
 	}
 }
