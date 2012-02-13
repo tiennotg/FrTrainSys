0a787f83f0b6b413cb517a5e67cd324a03dd24ca
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-14 00:23:27 +0100

    Add constructor and time constants.

--- Vacma.cs
+++ Vacma.cs
@@ -1,18 +1,25 @@
 using System;
-using OpenBveApi;
+using OpenBveApi.Runtime;
 
 namespace FrTrainSys
 {
 	public class Vacma: FrTrainDevice
 	{
+		private const int delayBeforeBreaking = 2500;
+		private const int delayBeforeRinging = 57500;
+		private Time globalTime;
+		private Time time;
+		
 		public Vacma (TrainSoundManager soundManager,
 		              TrainHandleManager handleManager,
 		              TrainControlManager controlManager): base(soundManager, handleManager, controlManager)
 		{
+			time = new Time(0);
 		}
 
-		public override void elapse (OpenBveApi.Runtime.ElapseData data)
+		public override void elapse (ElapseData data)
 		{
+			globalTime = data.TotalTime;
 		}
 
 		public override void reset ()
