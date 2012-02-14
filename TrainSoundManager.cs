9bf307a6e930476473e6734d88c7fcc9061778b3
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-14 21:54:00 +0100

    Add implementation for startSound and stopSound methods. Moving SoundIndex out of the TrainSoundManager class.

--- TrainSoundManager.cs
+++ TrainSoundManager.cs
@@ -1,27 +1,47 @@
 using System;
-using OpenBveApi;
+using OpenBveApi.Runtime;
+using System.Collections.Generic;
 
 namespace FrTrainSys
 {
-	public class TrainSoundManager
+	public enum SoundIndex
 	{
-		public enum SoundIndex
-		{
-			Vacma, VacmaRing, Beep, KvbOverSpeed, KvbClosedSignal
-		};
+		Vacma, VacmaRing, Beep, KvbOverSpeed, KvbClosedSignal
+	};
+	
+	public class TrainSoundManager
+	{		
+		private List<SoundHandle> loopSounds;
 
-		private OpenBveApi.Runtime.PlaySoundDelegate playSound;
+		private PlaySoundDelegate playSound;
 		private const double volume = 1.0;
 		private const double pitch = 1.0;
 
-		public TrainSoundManager (OpenBveApi.Runtime.PlaySoundDelegate playSound)
+		public TrainSoundManager (PlaySoundDelegate playSound)
 		{
 			this.playSound = playSound;
+			loopSounds = new List<SoundHandle>();
 		}
 
 		public void playSoundOnce (SoundIndex index)
 		{
 			playSound((int) index,volume,pitch,false);
+		}
+		
+		public int startSound(SoundIndex index)
+		{
+			SoundHandle handle = this.playSound((int) index,volume,pitch,true);
+			loopSounds.Add(handle);
+			return loopSounds.IndexOf(handle);
+		}
+		
+		public void stopSound(int id)
+		{
+			if (id < loopSounds.Count && id >= 0)
+			{
+				loopSounds[id].Stop();
+				loopSounds.RemoveAt(id);
+			}
 		}
 	}
 }
