1f2d2238aef760733faf8c41a1bc88e9970fd8ef
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-17 22:25:42 +0100

    Add constructor and reset method.

--- KVB.cs
+++ KVB.cs
@@ -5,25 +5,35 @@ namespace FrTrainSys
 {
 	public class KVB: FrTrainDevice
 	{
+		private enum GreenKvbAspects
+		{
+			none,double0, kvbp, running, l, p, b
+		};
+		
+		private enum YellowKvbAspects
+		{
+			none,double0, triple0, running, l, p
+		}
+		
 		public KVB (TrainSoundManager soundManager,
 		            TrainHandleManager handleManager,
 		            TrainControlManager controlManager): base(soundManager, handleManager, controlManager)
 		{
+			reset();
 		}
 		
 		public override void elapse (OpenBveApi.Runtime.ElapseData data)
 		{
-			
 		}
 		
 		public override void reset ()
 		{
-			
+			controlManager.setState(cabControls.GreenKVB, (int) GreenKvbAspects.none);
+			controlManager.setState(cabControls.YellowKVB, (int) YellowKvbAspects.triple0);
 		}
 		
 		public override void trainEvent (TrainEvent _event)
 		{
-			
 		}
 	}
 }
