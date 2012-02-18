a6e022589ed8b885b553b258c1a963fdd1286fa2
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-18 14:28:16 +0100

    Add new types of beacons.

--- Beacons.cs
+++ Beacons.cs
@@ -5,7 +5,12 @@ namespace FrTrainSys
 	/* Beacon list */
 	static class Beacons
 	{
-		public static int RepeatedSignal = 50;
+		public static int RepeatedSignal = 43000;
+		public static int Signal = 44000;
+		public static int SpeedLimit = 44100;
+		public static int TargetDistance = 44200;
+		public static int LandProfile = 44500;
+		public static int KvbControl = 45000;
 	}
 }
 
