58c6a1a4d9cfbad054f02c0b0a6e7cd8e4a93820
Guilhem Tiennot <guilhem.tiennot@gmail.com>
2012-02-18 14:28:29 +0100

    Add parametring method to KVB.

--- KVB.cs
+++ KVB.cs
@@ -5,6 +5,16 @@ namespace FrTrainSys
 {
 	public class KVB: FrTrainDevice
 	{
+		public enum TrainTypes
+		{
+			MA,ME,V
+		}
+		
+		private Speed maxSpeed;
+		private int trainLength;
+		private double decelCoeff;
+		private TrainTypes trainType;
+		
 		private enum GreenKvbAspects
 		{
 			none,double0, kvbp, running, l, p, b
@@ -22,14 +32,20 @@ namespace FrTrainSys
 			reset();
 		}
 		
+		public void setParameters (Speed maxSpeed, int trainLength, double decelCoeff, TrainTypes trainType)
+		{
+			this.maxSpeed = maxSpeed;
+			this.trainLength = trainLength;
+			this.decelCoeff = decelCoeff;
+			this.trainType = trainType;
+		}
+		
 		public override void elapse (OpenBveApi.Runtime.ElapseData data)
 		{
 		}
 		
 		public override void reset ()
 		{
-			controlManager.setState(cabControls.GreenKVB, (int) GreenKvbAspects.none);
-			controlManager.setState(cabControls.YellowKVB, (int) YellowKvbAspects.triple0);
 		}
 		
 		public override void trainEvent (TrainEvent _event)
